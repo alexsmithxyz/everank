435d82a30d371e35d4a395054c96d18ebb007344
Alex Smith <me@alexsmith.xyz>
2025-09-23 18:56:52 +0100

    add system spec for flash message layout

--- spec/system/flash_messages_spec.rb
+++ spec/system/flash_messages_spec.rb
@@ -3,17 +3,23 @@ require 'rails_helper'
 RSpec.describe "FlashMessages", type: :system do
   before do
     driven_by :selenium, using: :headless_firefox
-  end
 
-  it 'dismisses flash messages via the button' do
     visit sign_in_path
 
     click_button 'Sign in'
+  end
 
+  it 'dismisses flash messages via the button' do
     expect(page).to have_css('.flash.alert')
 
     click_button '×'
 
     expect(page).not_to have_css('.flash.alert')
+  end
+
+  it 'is layed out horizontally' do
+    within '.flash.alert' do
+      expect(page).to have_button('×', right_of: find('p'))
+    end
   end
 end
